86ae729ba7205cffb3172e2de2c62cbdbac76f45
tenfifty <tenfifty@Umarus-MacBook-Pro.local>
2023-04-27 16:31:42 +0700

    Fixed bug

--- lib/src/ifarms_input.dart
+++ lib/src/ifarms_input.dart
@@ -19,103 +19,108 @@ class IFInput {
     Function()? suffixAction,
     TextInputType? inputType = TextInputType.text,
   }) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            if (title != null) Text(title, style: IFTheme.textStyle.bodyReg),
-            if (!required!)
-              Text(
-                '(Jika ada)',
-                style: IFTheme.textStyle.bodyReg.copyWith(
-                  color: IFTheme.color.grey,
+    return Material(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              if (title != null) Text(title, style: IFTheme.textStyle.bodyReg),
+              if (!required!)
+                Text(
+                  '(Jika ada)',
+                  style: IFTheme.textStyle.bodyReg.copyWith(
+                    color: IFTheme.color.grey,
+                  ),
                 ),
+            ],
+          ),
+          // Test
+          TextFormField(
+            maxLines: 5,
+            minLines: longText! ? 5 : 1,
+            controller: controller,
+            initialValue: initialValue,
+            keyboardType: inputType,
+            validator: validator,
+            readOnly: readOnly!,
+            enabled: enabled,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor:
+                  enabled! ? IFTheme.color.white : IFTheme.color.lightGrey,
+              labelStyle: TextStyle(color: IFTheme.color.black),
+              hintText: hint,
+              hintStyle: IFTheme.textStyle.h2Reg.copyWith(
+                color: IFTheme.color.grey,
               ),
-          ],
-        ),
-        // Test
-        TextFormField(
-          maxLines: 5,
-          minLines: longText! ? 5 : 1,
-          controller: controller,
-          initialValue: initialValue,
-          keyboardType: inputType,
-          validator: validator,
-          readOnly: readOnly!,
-          enabled: enabled,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: enabled! ? IFTheme.color.white : IFTheme.color.lightGrey,
-            labelStyle: TextStyle(color: IFTheme.color.black),
-            hintText: hint,
-            hintStyle: IFTheme.textStyle.h2Reg.copyWith(
-              color: IFTheme.color.grey,
-            ),
-            contentPadding: const EdgeInsets.all(15.0),
-            errorStyle: IFTheme.textStyle.smallReg.copyWith(
-              color: IFTheme.color.red,
-            ),
-            prefixIcon: showPrefix!
-                ? Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Container(
-                      padding: EdgeInsets.all(ScreenUtil().setSp(15)),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          right: BorderSide(color: IFTheme.color.lightGrey),
+              contentPadding: const EdgeInsets.all(15.0),
+              errorStyle: IFTheme.textStyle.smallReg.copyWith(
+                color: IFTheme.color.red,
+              ),
+              prefixIcon: showPrefix!
+                  ? Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Container(
+                        padding: EdgeInsets.all(ScreenUtil().setSp(15)),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            right: BorderSide(color: IFTheme.color.lightGrey),
+                          ),
+                        ),
+                        child: SvgPicture.asset(
+                          prefixIcon!,
+                          height: ScreenUtil().setHeight(20),
+                          width: ScreenUtil().setWidth(20),
+                          colorFilter: ColorFilter.mode(
+                            IFTheme.color.blue,
+                            BlendMode.srcIn,
+                          ),
                         ),
                       ),
-                      child: SvgPicture.asset(
-                        prefixIcon!,
+                    )
+                  : null,
+              suffixIcon: showSuffix!
+                  ? IconButton(
+                      onPressed: () => suffixAction,
+                      icon: SvgPicture.asset(
+                        showClearbutton!
+                            ? 'assets/svgs/close-circle.svg'
+                            : suffixIcon ?? 'assets/svgs/close-circle.svg',
                         height: ScreenUtil().setHeight(20),
-                        width: ScreenUtil().setWidth(20),
                         colorFilter: ColorFilter.mode(
-                          IFTheme.color.blue,
+                          IFTheme.color.grey,
                           BlendMode.srcIn,
                         ),
                       ),
-                    ),
-                  )
-                : null,
-            suffixIcon: showSuffix!
-                ? IconButton(
-                    onPressed: () => suffixAction,
-                    icon: SvgPicture.asset(
-                      showClearbutton!
-                          ? 'assets/svgs/close-circle.svg'
-                          : suffixIcon ?? 'assets/svgs/close-circle.svg',
-                      height: ScreenUtil().setHeight(20),
-                      colorFilter: ColorFilter.mode(
-                        IFTheme.color.grey,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  )
-                : null,
-            alignLabelWithHint: true,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide(width: 1, color: IFTheme.color.lightGrey),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide(width: 1, color: IFTheme.color.lightGrey),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide(width: 1, color: IFTheme.color.red),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide(
-                color: IFTheme.color.blue,
+                    )
+                  : null,
+              alignLabelWithHint: true,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide:
+                    BorderSide(width: 1, color: IFTheme.color.lightGrey),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide:
+                    BorderSide(width: 1, color: IFTheme.color.lightGrey),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(width: 1, color: IFTheme.color.red),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(
+                  color: IFTheme.color.blue,
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
