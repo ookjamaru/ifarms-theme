690a4e4a9bd06cc117916adc1dd48e39d4dd0974
Renaldi Kumar Mahendra <renaldi1050@gmail.com>
2023-11-27 15:37:41 +0700

    Dropdown multi

--- lib/src/ifarms_input.dart
+++ lib/src/ifarms_input.dart
@@ -304,7 +304,6 @@ class IFInput {
               backgroundColor: IFTheme.color.white,
             ),
             searchFieldProps: TextFieldProps(
-              padding: const EdgeInsets.all(5),
               decoration: _inputDecoration.copyWith(hintText: hintText ?? "Cari disini..."),
             ),
             loadingBuilder: loadingBuilder,
